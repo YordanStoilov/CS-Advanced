70bf7a6f0453459c3bf10aa6e480565aac0e29da
YordanStoilov <iordan27@gmail.com>
2024-08-31 20:35:47 +0300

    solved task 3

---
+++ 04.StreamsFilesAndDirectories/WordCount/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> wordOccurences = new();
+            using (StreamReader reader = new StreamReader(@"./words.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string[] words = reader.ReadLine().Split();
+                    foreach (string word in words)
+                    {
+                        wordOccurences[word] = 0;
+                    }
+                }
+            }
+            using (StreamReader reader = new StreamReader(@"./text.txt"))
+            {
+                char[] delimiters = new char[] { ',', '.', '!', '?', '-', ' ' };
+                while (!reader.EndOfStream)
+                {
+                    string[] words = reader.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string word in words)
+                    {
+                        foreach (var kvp in wordOccurences)
+                        {
+                            if (word.Contains(kvp.Key))
+                            {
+                                wordOccurences[kvp.Key]++;
+                            }
+                        }
+                    }
+                }
+            }
+            Dictionary<string, int> sorted = wordOccurences.OrderByDescending(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            using (StreamWriter writer = new StreamWriter(@"./output.txt"))
+            {
+                foreach (var (word, occurences) in sorted)
+                {
+                    writer.WriteLine($"{word} - {occurences}");
+                }
+            }
+        }
+    }
+}
