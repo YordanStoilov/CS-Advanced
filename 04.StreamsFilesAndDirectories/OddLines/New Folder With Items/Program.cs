bfc02725261d03c1d5a13ce2a7e8b1f9338a4739
YordanStoilov <iordan27@gmail.com>
2024-08-31 19:41:36 +0300

    solved task 1

--- 04.StreamsFilesAndDirectories/OddLines/New Folder With Items/Program.cs
+++ 04.StreamsFilesAndDirectories/OddLines/New Folder With Items/Program.cs
@@ -1,41 +1,35 @@
-﻿namespace OddLines
+﻿using System.IO;
+
+namespace OddLines
 {
     public class OddLines
     {
         static void Main()
         {
-
             string inputFilePath = @"..\..\..\Files\input.txt";
             string outputFilePath = @"..\..\..\Files\output.txt";
-            // string inputFilePath = @"./input.txt";
-            // string outputFilePath = @"./output.txt";
 
             ExtractOddLines(inputFilePath, outputFilePath);
+        }
 
-
-            static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(inputFilePath))
             {
-                int count = 0;
-                using (StreamReader reader = new StreamReader(inputFilePath))
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
                 {
-                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    while (!reader.EndOfStream)
                     {
-                        while (!reader.EndOfStream)
+                        string line = reader.ReadLine();
+                        if (count % 2 == 1)
                         {
-                            string line = reader.ReadLine();
-                            if (count % 2 == 1)
-                            {
-                                writer.WriteLine(line);
-                            }
-                            count++;
+                            writer.WriteLine(line);
                         }
+                        count++;
                     }
-
                 }
             }
-
         }
     }
 }
-
-//Test
