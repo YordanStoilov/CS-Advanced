2c8c99e8eb7293b49a2688763c0a2f0867321628
YordanStoilov <iordan27@gmail.com>
2024-09-01 14:52:29 +0300

    solved task 5

--- 04.StreamsFilesAndDirectories/ExtractSpecialBytes/Program.cs
+++ 04.StreamsFilesAndDirectories/ExtractSpecialBytes/Program.cs
@@ -8,13 +8,40 @@ namespace ExtractSpecialBytes
             string binaryFilePath = @"..\..\..\Files\example.png";
             string bytesFilePath = @"..\..\..\Files\bytes.txt";
             string outputPath = @"..\..\..\Files\output.bin";
+            // string binaryFilePath = @"./example.png";
+            // string bytesFilePath = @"./bytes.txt";
+            // string outputPath = @"./output.bin";
 
             ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
         }
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
+            List<byte> bytes = new List<byte>();
+            using (StreamReader reader = new StreamReader(bytesFilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    byte currentByte = byte.Parse(reader.ReadLine());
+                    bytes.Add(currentByte);
+                }
+            }
+            using (FileStream stream = new FileStream(binaryFilePath, FileMode.Open))
+            {
+                byte[] data = new byte[stream.Length];
+                stream.Read(data, 0, data.Length);
 
+                using (FileStream fs = new FileStream("output.bin", FileMode.Create, FileAccess.Write))
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        byte currentByte = data[i];
+                        if (bytes.Contains(currentByte))
+                        {
+                            bw.Write(currentByte);
+                        }
+                    }
+            }
         }
     }
 }
