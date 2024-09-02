118cfcac4d9534c155a6b5459cd86f249a3e2774
YordanStoilov <iordan27@gmail.com>
2024-09-02 11:55:40 +0300

    started task 2

---
+++ 04.StreamsFilesAndDirectories/LineNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            // string inputFilePath = @"..\..\..\text.txt";
+            // string outputFilePath = @"..\..\..\output.txt";
+            string inputFilePath = @"./text.txt";
+            string outputFilePath = @"./output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                char[] punctuationMarks = new char[] { '.', ',', '!', '?', '-', ';', ':', '(', ')', '\'', '"' };
+                int count = 1;
+                string line = reader.ReadLine();
+                int punctuationCount = 0;
+                int charsCount = 0;
+
+                foreach (char character in line)
+                {
+                    if (character == ' ')
+                    {
+                        continue;
+                    }
+                    if (punctuationMarks.Contains(character))
+                    {
+                        punctuationCount++;
+                    }
+                    else
+                    {
+                        charsCount++;
+                    }
+                }
+            }
+        }
+    }
+}
