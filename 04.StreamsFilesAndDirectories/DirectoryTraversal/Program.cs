703c9d7ce27a220fb31bb6dac612eb02eb76ce0c
YordanStoilov <iordan27@gmail.com>
2024-09-02 14:37:37 +0300

    solved task 4

--- 04.StreamsFilesAndDirectories/DirectoryTraversal/Program.cs
+++ 04.StreamsFilesAndDirectories/DirectoryTraversal/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace DirectoryTraversal
+{
+    using System;
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            Dictionary<string, List<FileInfo>> fileDictionary = TraverseDirectory(path);
+
+            WriteReportToDesktop(fileDictionary, reportFileName);
+        }
+
+        public static Dictionary<string, List<FileInfo>> TraverseDirectory(string inputFolderPath)
+        {
+            Dictionary<string, List<FileInfo>> fileDictionary = new();
+            string[] files = Directory.GetFiles(inputFolderPath);
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                if (fileInfo.Exists)
+                {
+                    string extension = fileInfo.Extension;
+                    if (!fileDictionary.ContainsKey(extension))
+                    {
+                        fileDictionary[extension] = new List<FileInfo>();
+                    }
+                    fileDictionary[extension].Add(fileInfo);
+                }
+            }
+            return fileDictionary;
+        }
+
+        public static void WriteReportToDesktop(Dictionary<string, List<FileInfo>> fileDictionary, string reportFileName)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach (var kvp in fileDictionary.OrderByDescending(item => item.Value.Count).ThenBy(item => item.Key))
+                {
+                    writer.WriteLine($"{kvp.Key}");
+                    foreach (FileInfo file in kvp.Value.OrderByDescending(file => file.Length))
+                    {
+                        writer.WriteLine($"--{file.Name} - {(double)file.Length / 1024}kb");
+                    }
+                }
+            }
+        }
+    }
+}
