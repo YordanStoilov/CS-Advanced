cd487eb266b5baa5758cd9dd82d6da5b021768b5
YordanStoilov <iordan27@gmail.com>
2024-09-03 22:15:17 +0300

    removed top level statements

--- 05.FunctionalProgramming/AppliedArithmetics/Program.cs
+++ 05.FunctionalProgramming/AppliedArithmetics/Program.cs
@@ -1,40 +1,35 @@
-﻿namespace AppliedArithmetics
+﻿
+static void Main(string[] args)
 {
-    internal class Program
+    Action<int[]> PrintArray = array => Console.WriteLine(string.Join(" ", array));
+
+    int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse).ToArray();
+
+    while (true)
     {
-        static void Main(string[] args)
+        Func<int, int> action = x => x;
+        string input = Console.ReadLine();
+        if (input.ToLower() == "end")
         {
-            Action<int[]> PrintArray = array => Console.WriteLine(string.Join(" ", array));
-
-            int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse).ToArray();
-
-            while (true)
-            {
-                Func<int, int> action = x => x;
-                string input = Console.ReadLine();
-                if (input.ToLower() == "end")
-                {
-                    break;
-                }
-                if (input.ToLower() == "add")
-                {
-                    action = x => x + 1;
-                }
-                else if (input.ToLower() == "multiply")
-                {
-                    action = x => x * 2;
-                }
-                else if (input.ToLower() == "subtract")
-                {
-                    action = x => x - 1;
-                }
-                else if (input.ToLower() == "print")
-                {
-                    PrintArray(nums);
-                }
-                nums = nums.Select(x => action(x)).ToArray();
-            }
+            break;
         }
+        if (input.ToLower() == "add")
+        {
+            action = x => x + 1;
+        }
+        else if (input.ToLower() == "multiply")
+        {
+            action = x => x * 2;
+        }
+        else if (input.ToLower() == "subtract")
+        {
+            action = x => x - 1;
+        }
+        else if (input.ToLower() == "print")
+        {
+            PrintArray(nums);
+        }
+        nums = nums.Select(x => action(x)).ToArray();
     }
 }
