4a2f238ae2c6e4f37c71a2c12d46a2ae1af627ff
YordanStoilov <iordan27@gmail.com>
2024-09-03 17:41:17 +0300

    solved task 3

--- 05.FunctionalProgramming/CustomMinFunction/Program.cs
+++ 05.FunctionalProgramming/CustomMinFunction/Program.cs
@@ -1,2 +1,8 @@
 ﻿
+Func<List<int>, int> FindMin = list => list.Min();
 
+List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+.Select(int.Parse)
+.ToList();
+
+Console.WriteLine(FindMin(nums));
--- 05.FunctionalProgramming/CustomMinFunction/obj/Debug/net8.0/CustomMinFunction.AssemblyInfo.cs
+++ 05.FunctionalProgramming/CustomMinFunction/obj/Debug/net8.0/CustomMinFunction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomMinFunction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53f166afc6613c14cb0a453abeaa69e9b59bd667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4be4f88ddbce67dcaebb96963449f1fd8293500")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomMinFunction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomMinFunction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
