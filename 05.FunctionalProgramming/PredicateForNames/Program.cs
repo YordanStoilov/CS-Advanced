9653780d3d88518ff633ba334a81cf8cf65c701c
YordanStoilov <iordan27@gmail.com>
2024-09-03 22:32:22 +0300

    solved task 7

---
+++ 05.FunctionalProgramming/PredicateForNames/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+string[] names = Console.ReadLine().Split();
+
+Predicate<string> predicate = name => name.Length <= n;
+Action<string[]> PrintNames = names => Console.WriteLine(string.Join("\n", names));
+
+names = names.Where(name => predicate(name)).ToArray();
+PrintNames(names);
