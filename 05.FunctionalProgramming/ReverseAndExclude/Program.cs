57592deb77f3ef1c557482aa27f3848b15fc336a
YordanStoilov <iordan27@gmail.com>
2024-09-03 22:26:27 +0300

    solved task 6

---
+++ 05.FunctionalProgramming/ReverseAndExclude/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = int.Parse(Console.ReadLine());
+
+Func<int[], int[]> reverseArray = array => array.Reverse().ToArray();
+Predicate<int> predicate = x => x % n != 0;
+Action<int[]> PrintArray = array => Console.WriteLine(string.Join(" ", array));
+
+nums = reverseArray(nums).Where(x => predicate(x)).ToArray();
+PrintArray(nums);
