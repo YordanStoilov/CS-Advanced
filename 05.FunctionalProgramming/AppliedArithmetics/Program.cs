46fcb16433bcab4986e2b3db6b263c3f3802cdfa
YordanStoilov <iordan27@gmail.com>
2024-09-03 22:14:57 +0300

    solved task 5

--- 05.FunctionalProgramming/AppliedArithmetics/Program.cs
+++ 05.FunctionalProgramming/AppliedArithmetics/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿namespace AppliedArithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int[]> PrintArray = array => Console.WriteLine(string.Join(" ", array));
+
+            int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            while (true)
+            {
+                Func<int, int> action = x => x;
+                string input = Console.ReadLine();
+                if (input.ToLower() == "end")
+                {
+                    break;
+                }
+                if (input.ToLower() == "add")
+                {
+                    action = x => x + 1;
+                }
+                else if (input.ToLower() == "multiply")
+                {
+                    action = x => x * 2;
+                }
+                else if (input.ToLower() == "subtract")
+                {
+                    action = x => x - 1;
+                }
+                else if (input.ToLower() == "print")
+                {
+                    PrintArray(nums);
+                }
+                nums = nums.Select(x => action(x)).ToArray();
+            }
+        }
+    }
+}
--- 05.FunctionalProgramming/AppliedArithmetics/obj/Debug/net8.0/AppliedArithmetics.AssemblyInfo.cs
+++ 05.FunctionalProgramming/AppliedArithmetics/obj/Debug/net8.0/AppliedArithmetics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppliedArithmetics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f68d1c71f8cb191ebc671343162c6b6ae9bc9940")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb5468b9fe856fe5ee16df4722c7cc15dcd0b2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppliedArithmetics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppliedArithmetics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
