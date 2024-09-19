56ec380ea1413aa3b0287ccac50cbae31c237476
YordanStoilov <iordan27@gmail.com>
2024-09-19 12:15:00 +0300

    solved task 2

--- 06.DefiningClasses/CarManafacturer/obj/Debug/net6.0/CarManafacturer.AssemblyInfo.cs
+++ 06.DefiningClasses/CarManafacturer/obj/Debug/net6.0/CarManafacturer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarManafacturer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95af9f6bcdeb362c8e49353bd1faae8a2c862b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0094b3bea55b8a08f836dff099958ed9bfd523")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarManafacturer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarManafacturer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
