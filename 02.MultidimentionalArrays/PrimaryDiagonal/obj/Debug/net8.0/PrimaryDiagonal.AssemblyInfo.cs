00a5778b012256a16bd30e617cda414956f3815e
YordanStoilov <iordan27@gmail.com>
2024-09-23 11:43:46 +0300

    solved task 9

--- 02.MultidimentionalArrays/PrimaryDiagonal/obj/Debug/net8.0/PrimaryDiagonal.AssemblyInfo.cs
+++ 02.MultidimentionalArrays/PrimaryDiagonal/obj/Debug/net8.0/PrimaryDiagonal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimaryDiagonal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6344d60d102d80bd9038f67a4c397b2f5a5d1b13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48db8c7b91bb7567578a61c68dd1fa5342798438")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimaryDiagonal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimaryDiagonal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ PokemonTrainer/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace PokemonTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new();
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (input == "Tournament")
+                    {
+                        break;
+                    }
+                    string[] line = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    string trainerName = line[0];
+                    string pokemonName = line[1];
+                    string pokemonElement = line[2];
+                    int pokemonHealth = int.Parse(line[3]);
+
+                    Trainer[] match = trainers.Where(tr => tr.Name == trainerName).ToArray();
+                    Trainer currentTrainer;
+
+                    if (match.Length == 0)
+                    {
+                        currentTrainer = new Trainer(trainerName);
+                        trainers.Add(currentTrainer);
+                    }
+                    else
+                    {
+                        currentTrainer = match[0];
+                    }
+                    currentTrainer.Pokemons.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+                }
+                while (true)
+                {
+                    string line = Console.ReadLine();
+                    if (line == "End")
+                    {
+                        break;
+                    }
+
+                    foreach (Trainer trainer in trainers)
+                    {
+                        bool hasPokemonWithSameElement = false;
+
+                        foreach (Pokemon pokemon in trainer.Pokemons)
+                        {
+                            if (pokemon.Element == line)
+                            {
+                                hasPokemonWithSameElement = true;
+                            }
+                        }
+                        if (hasPokemonWithSameElement)
+                        {
+                            trainer.NumberOfBadges++;
+                        }
+                        else
+                        {
+                            List<Pokemon> alivePokemons = new();
+                            foreach (Pokemon pokemon in trainer.Pokemons)
+                            {
+                                pokemon.Health -= 10;
+                                if (pokemon.Health > 0)
+                                {
+                                    alivePokemons.Add(pokemon);
+                                }
+                            }
+                            trainer.Pokemons = alivePokemons.ToList();
+                        }
+                    }
+                }
+                Trainer[] sorted = trainers.OrderByDescending(tr => tr.NumberOfBadges).ToArray();
+
+                foreach(Trainer trainer in sorted)
+                {
+                    Console.WriteLine(trainer);
+                }
+            }
+        }
+    }
+}
