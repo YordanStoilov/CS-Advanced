5820b4a2a1eeb4a887c32ed55559a5a011509bf3
YordanStoilov <iordan27@gmail.com>
2024-09-27 15:03:22 +0300

    implemented RemoveAt method

--- ImplementingStacksAndQueues/Program.cs
+++ ImplementingStacksAndQueues/Program.cs
@@ -9,6 +9,7 @@ namespace ImplementingStacksAndQueues
             list.Add(1);
             list.Add(2);
             list.Add(3);
+            list.Add(4);
             
             list.RemoveAt(0);
         }
