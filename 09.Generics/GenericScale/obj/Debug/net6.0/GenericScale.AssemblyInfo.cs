f228268de890ac2e0674e21bc13e315d8f587ff4
YordanStoilov <iordan27@gmail.com>
2024-10-06 12:02:34 +0300

    solved task 1 from IEnumerable Lab

--- 09.Generics/GenericScale/obj/Debug/net6.0/GenericScale.AssemblyInfo.cs
+++ 09.Generics/GenericScale/obj/Debug/net6.0/GenericScale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GenericScale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc767bc383bd8c34986a47740364de28ea813e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3bb2b28c174aa0c16a7c8e301e7270e93a64ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("GenericScale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GenericScale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 10.IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators;
+public class Library : IEnumerable<Book>
+{
+    private List<Book>? Books;
+
+    public Library(params Book[] books)
+    {
+        Books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator()
+    {
+        for (int i = 0; i < Books.Count; i++)
+        {
+            yield return Books[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
