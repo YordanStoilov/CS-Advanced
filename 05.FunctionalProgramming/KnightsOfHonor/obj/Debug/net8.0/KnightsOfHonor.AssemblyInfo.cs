657b5620776d6a262a85c594f1cdc9063d2462ff
YordanStoilov <iordan27@gmail.com>
2024-10-15 15:02:55 +0300

    solved task 2 from exam preparation 2

--- 05.FunctionalProgramming/KnightsOfHonor/obj/Debug/net8.0/KnightsOfHonor.AssemblyInfo.cs
+++ 05.FunctionalProgramming/KnightsOfHonor/obj/Debug/net8.0/KnightsOfHonor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnightsOfHonor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357c33aface812f12cf5b2e66526bf760d1310b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837c4e174f6771a14af34c67cc8f53acbdb37281")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnightsOfHonor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KnightsOfHonor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 12.ExamPreparation/MouseInTheKitchen/Program.cs
@@ -0,0 +1,107 @@
+﻿
+
+string[] dimensions = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+int rows = int.Parse(dimensions[0]), cols = int.Parse(dimensions[1]);
+
+int[,] matrix = new int[rows, cols];
+int mouseRow = 0;
+int mouseCol = 0;
+
+int totalCheese = 0;
+int cheeseEaten = 0;
+
+for (int row = 0; row < rows; row++)
+{
+    string line = Console.ReadLine();
+
+    for (int col = 0; col < line.Length; col++)
+    {
+        matrix[row, col] = line[col];
+        if (matrix[row, col] == 'M')
+        {
+            mouseRow = row;
+            mouseCol = col;
+        }
+        else if (matrix[row, col] == 'C')
+        {
+            totalCheese++;
+        }
+    }
+}
+
+
+while (true)
+{
+    string command = Console.ReadLine();
+    if (command == "danger")
+    {
+        Console.WriteLine("Mouse will come back later!");
+        break;
+    }
+
+    int[] nextPosition = new int[] { mouseRow, mouseCol };
+
+    switch (command)
+    {
+        default:
+            break;
+
+        case "up":
+            nextPosition[0]--;
+            break;
+
+        case "down":
+            nextPosition[0]++;
+            break;
+
+        case "left":
+            nextPosition[1]--;
+            break;
+
+        case "right":
+            nextPosition[1]++;
+            break;
+    }
+
+    if (nextPosition[0] < 0 || nextPosition[0] >= rows ||  nextPosition[1] < 0 || nextPosition[1] >= cols)
+    {
+        Console.WriteLine("No more cheese for tonight!");
+        break;
+    }
+
+    char nextChar = (char)matrix[nextPosition[0], nextPosition[1]];
+
+    if (nextChar == '@')
+    {
+        continue;
+    }
+
+    matrix[mouseRow, mouseCol] = '*';
+    mouseRow = nextPosition[0]; mouseCol = nextPosition[1];
+    matrix[mouseRow, mouseCol] = 'M';
+
+    if (nextChar == 'C')
+    {
+        cheeseEaten++;
+    }
+    else if (nextChar == 'T')
+    {
+        Console.WriteLine("Mouse is trapped!");
+        break;
+    }
+
+    if (cheeseEaten >= totalCheese)
+    {
+        Console.WriteLine("Happy mouse! All the cheese is eaten, good night!");
+        break;
+    }
+}
+
+for (int row = 0; row < rows; row++)
+{
+    for (int col = 0; col < cols; col++)
+    {
+        Console.Write((char)matrix[row, col]);
+    }
+    Console.WriteLine();
+}
