65ffc66443198528b3038a5d5c96fc3919a1e7e1
YordanStoilov <iordan27@gmail.com>
2024-10-18 17:10:00 +0300

    solved task 1 from exam prep 7

---
+++ 18.ExamPreparation/ChickenSnack/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+Queue<int> armors = new Queue<int>(Console.ReadLine().Split(",", 
+    StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+Stack<int> attacks = new Stack<int>(Console.ReadLine().Split(",",
+    StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+int killedMonsters = 0;
+
+while (armors.Count > 0 && attacks.Count > 0)
+{
+    int armor = armors.Dequeue();
+    int attack = attacks.Pop();
+    int difference = attack - armor;
+
+    if (difference >= 0)
+    {
+        killedMonsters++;
+        if (difference == 0)
+        {
+            continue;
+        }
+        attack = difference;
+        if (attacks.Count > 0)
+        {
+            attacks.Push(attacks.Pop() + difference);
+        }
+        else
+        {
+            attacks.Push(difference);
+        }
+    }
+    else if (difference < 0)
+    {
+        armors.Enqueue(Math.Abs(difference));
+    }
+}
+
+if (armors.Count <= 0)
+{
+    Console.WriteLine("All monsters have been killed!");
+}
+else if (attacks.Count <= 0)
+{
+    Console.WriteLine("The soldier has been defeated.");
+}
+
+Console.WriteLine($"Total monsters killed: {killedMonsters}");
